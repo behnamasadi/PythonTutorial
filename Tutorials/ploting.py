58ca374f72de9f0e316b6ab89ef6ad58745cfa3b
Behnam Asadi <behnam.asadi@gmail.com>
2023-02-28 22:20:50 +0100

    plot updated

--- Tutorials/ploting.py
+++ Tutorials/ploting.py
@@ -342,3 +342,33 @@ Z=20*X**2+W**2
 levels=np.arange(np.min(Z), np.max(Z),10)
 plt.contourf(X,W,Z,levels=levels)
 plt.show()
+
+####################################### quiver #######################################
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+
+# [x_start, y_start, z_start, x_length, y_length, z_length]
+soa = np.array([[0, 0, 1, 1, -2, 0], [0, 0, 2, 1, 1, 0],
+                [0, 0, 3, 2, 1, 0], [0, 0, 4, 0.5, 0.7, 0]])\
+
+
+
+X, Y, Z, U, V, W = zip(*soa)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(X, Y, Z, U, V, W,length=.005)
+
+
+ax.autoscale(enable=False, axis='both', tight=None)
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.show()
+
